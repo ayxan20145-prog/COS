df052d5af9913a43cbca28c3abc46178b810d788
ayxan <ayxan.20145@gmail.com>
2026-09-17 20:49:47 +0400

    added a print function

--- src/main.c
+++ src/main.c
@@ -1,8 +1,18 @@
 char *vga = (char *)0xb8000;
+int row = 0;
+int column = 0;
 
+void print(char byte) {
+  int position = row * 80 + column;
+
+  vga[position * 2] = byte;
+  vga[position * 2 + 1] = 0x0f;
+
+  column++;
+}
 void kernel_main(void) {
-  vga[0] = 'a';
-  vga[1] = 0x0f;
+  print('h');
+  print('i');
 
   while(1) {}
 }
