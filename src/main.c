a52efd6baeb32ca723778c7abe1ef66b8dfa8174
ayxan <ayxan.20145@gmail.com>
2026-09-17 20:52:04 +0400

    made print handle new line and end of row

--- src/main.c
+++ src/main.c
@@ -3,6 +3,17 @@ int row = 0;
 int column = 0;
 
 void print(char byte) {
+  if(byte == '\n') {
+    column = 0;
+    row++;
+    return;
+  }
+
+  if(column >= 80) {
+    column = 0;
+    row++;
+  }
+
   int position = row * 80 + column;
 
   vga[position * 2] = byte;
@@ -12,6 +23,7 @@ void print(char byte) {
 }
 void kernel_main(void) {
   print('h');
+  print('\n');
   print('i');
 
   while(1) {}
