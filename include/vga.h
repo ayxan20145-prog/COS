2ee0752e50e70caf47db682c5afcbcc992463bfc
ayxan <ayxan.20145@gmail.com>
2026-09-17 21:05:15 +0400

    added include dir and added vga.h and vga.c

---
+++ include/vga.h
@@ -0,0 +1,8 @@
+#ifndef VGA_H
+#define VGA_H
+
+void print_byte(char byte);
+void print_string(char str[]);
+void clear(void);
+
+#endif
--- src/main.c
+++ src/main.c
@@ -1,37 +1,5 @@
-char *vga = (char *)0xb8000;
-int row = 0;
-int column = 0;
+#include "vga.h"
 
-void print_byte(char byte) {
-  if(byte == '\n') {
-    column = 0;
-    row++;
-    return;
-  }
-
-  if(column >= 80) {
-    column = 0;
-    row++;
-  }
-
-  int position = row * 80 + column;
-
-  vga[position * 2] = byte;
-  vga[position * 2 + 1] = 0x0f;
-
-  column++;
-}
-void print_string(char str[]) {
-  for(int i = 0; str[i] != '\0'; i++) {
-    print_byte(str[i]);
-  }
-}
-void clear(void) {
-  for(int i = 0; i < 2000; i++) {
-    vga[i * 2] = ' ';
-    vga[i * 2 + 1] = 0x0f;
-  }
-}
 void kernel_main(void) {
   clear();
   print_string("hi\nhello");
